04e14002058b7f845d9ebf7dfd18f2c0e60673e6
Mircea Cristian <mircea.gordan@gmail.com>
2020-12-20 00:23:34 +0100

    Added first trial

---
+++ TodoApi-1234567890987654321/Semaphore.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace TodoApi
+{
+    public class Semaphore
+    {
+        public string Id { get; set; }
+
+        public string Country { get; set; }
+
+        public string CabinetName { get; set; }
+
+        public string SemaphoreColor { get; set; }
+
+        public string SemaphoreColorLocation { get; set; }
+
+
+
+        public string Summary { get; set; }
+    }
+}
